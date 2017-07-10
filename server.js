39a3923ae90c31126d9495c526a7e57b87d47e6f
Sam Corin <samcorin@gmail.com>
2017-07-11 08:54:03 +0900

    installed express-sslify

--- server.js
+++ server.js
@@ -5,16 +5,18 @@ var server = require('http').Server(app);
 var cors = require('cors');
 var helmet = require('helmet');
 var compression = require('compression');
+var enforce = require('express-sslify');
 
 app.use(cors());
 app.use(compression())
 app.use(helmet())
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
 
 // Is this necessary?
-app.use(helmet.hsts({
-  maxAge: 31536000,
-  includeSubDomains: true
-}));
+// app.use(helmet.hsts({
+//   maxAge: 31536000,
+//   includeSubDomains: true
+// }));
 
 app.use(express.static(__dirname + '/build'));
 
