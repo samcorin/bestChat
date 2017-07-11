fac7ce437b22d3aa2bfee88c076c9ab37d6dad98
Sam Corin <samcorin@gmail.com>
2017-07-11 11:16:54 +0900

    initMiit basic functions

--- src/components/Home/ConversationNavBar.js
+++ src/components/Home/ConversationNavBar.js
@@ -1,30 +1,42 @@
 import React, {Component} from 'react';
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, Redirect} from 'react-router-dom'
 import './ConversationNavBar.css'
 import FaArrowLeft from 'react-icons/lib/fa/arrow-left';
 import dropdown from './../../utils/dropdown';
 
 class ConversationNavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false
+    }
+  }
   // <img src={`https://api.adorable.io/avatars/60/${propsRef.roomId}@adorable.io.png`} className="previewPhoto" />
 
   componentDidMount() {
-    dropdown();
+    dropdown.init();
     const call = document.getElementById('initCall');
-    const miit = document.getElementById('initCall');
+    const miit = document.getElementById('initMiit');
 
     const startCall = () => {
       console.log("start call to: ", this.props.room)
       // <NavLink className="LinkStyle" to="/Miit"><FaArrowLeft /></NavLink>
       // webrtc stuff.
+
     }
 
     const startMiit = () => {
       console.log("start MIIT to: ", this.props.room)
+      // do something about dropdown?
+      this.setState({
+        redirect: true
+      })
       // <NavLink className="LinkStyle" to="/Miit"><FaArrowLeft /></NavLink>
 
       // show a new screen? or go straight to miit?
       // 1. get currentUser coords?
       // 2. get other users coords?
+      // browserHistory.push('/Miit')
     }
 
 
@@ -33,14 +45,17 @@ class ConversationNavBar extends Component {
     call.addEventListener('click', startCall, false);
 
     // Event listeners for Miit
-    call.addEventListener('touchstart', startMiit, false);
-    call.addEventListener('click', startMiit, false);
+    miit.addEventListener('touchstart', startMiit, false);
+    miit.addEventListener('click', startMiit, false);
 
   }
 
 
 
   render() {
+    if (this.state.redirect) {
+      return <Redirect push to='/Miit' />;
+    }
     return (
       <div style={{backgroundColor: '#2196F3'}} id="ConversationNavBar">
         <NavLink id="backButton" className="LinkStyle" to="/"><FaArrowLeft /></NavLink>
